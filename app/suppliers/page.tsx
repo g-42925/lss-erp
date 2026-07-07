eb414afcc3f96d59ea35752b33847c668ad1f37d
g-42925 <ecipersia@gmail.com>
2026-07-07 10:10:44 +0700

    refactor: simplify suppliers table layout and update modal styling

--- app/suppliers/page.tsx
+++ app/suppliers/page.tsx
@@ -175,12 +175,6 @@ export default function Suppliers() {
     <>
       <div className="h-full p-6 flex flex-col gap-3 text-black">
         <span className="text-2xl">Suppliers <span className="text-sm leading-loose">Manage your suppliers</span></span>
-        <label className="input w-full">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" />
-          </svg>
-          <input onKeyUp={(e) => search((e.target as HTMLInputElement).value)} type="search" required placeholder="Search" className="w-full p-3" />
-        </label>
         <div className="bg-white h-full border-t-4 border-blue-900 flex flex-col p-6 gap-6">
           <div className="flex flex-row">
             <span className="self-center">All your Suppliers</span>
@@ -190,43 +184,6 @@ export default function Suppliers() {
               </svg>
               Add
             </button>
-          </div>
-          <div className="flex flex-row">
-            <div className="flex flex-row gap-2 items-center flex-1">
-              Show
-              <select className="select w-16">
-                <option>20</option>
-                <option>30</option>
-                <option>40</option>
-              </select>
-              Entries
-            </div>
-            <div className="flex flex-row gap-1">
-              <div className="flex flex-row border-2 border-black p-1 rounded-md items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
-                </svg>
-                Export to csv
-              </div>
-              <div className="flex flex-row border-2 border-black p-1 rounded-md items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
-                </svg>
-                Export to excel
-              </div>
-              <div className="flex flex-row border-2 border-black p-1 rounded-md items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
-                </svg>
-                Export to pdf
-              </div>
-              <div className="flex flex-row border-2 border-black p-1 rounded-md items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0 1 10.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0 .229 2.523a1.125 1.125 0 0 1-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0 0 21 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 0 0-1.913-.247M6.34 18H5.25A2.25 2.25 0 0 1 3 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 0 1 1.913-.247m10.5 0a48.536 48.536 0 0 0-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5Zm-3 0h.008v.008H15V10.5Z" />
-                </svg>
-                Print
-              </div>
-            </div>
           </div>
           {
             getSuppliersFn.loading
@@ -245,13 +202,11 @@ export default function Suppliers() {
                   ?
                   <div className="overflow-x-auto">
                     <table className="table">
-                      <thead>
+                      <thead className="text-black">
                         <tr>
                           <th>Name</th>
                           <th>Email</th>
-                          <th>Tax Number</th>
-                          <th>Address</th>
-                          <th>Mobile</th>
+                          <th>Phone</th>
                           <th>...</th>
                         </tr>
                       </thead>
@@ -261,10 +216,8 @@ export default function Suppliers() {
                             return (
                               <tr key={s._id}>
                                 <td>{s.bussinessName}</td>
-                                <td>{s.email}</td>
-                                <td>{s.taxNumber}</td>
-                                <td>{s.address}</td>
-                                <td>{s.mobile}</td>
+                                <td className="max-w-[10ch] truncate">{s.email}</td>
+                                <td className="max-w-[10ch] truncate">{s.mobile}</td>
                                 <td>
                                   <button disabled={!isSuperAdmin && roleDetail.permission !== 'addandedit'} onClick={() => edit(s._id)}>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
@@ -320,30 +273,30 @@ export default function Suppliers() {
         </div>
       </div>
       <dialog id="my_modal_2" ref={editRef} className="modal text-black">
-        <div className="modal-box">
+        <div className="modal-box bg-white">
           <div className="flex flex-col gap-3">
             <span className="text-2xl">Edit Supplier</span>
             <form onSubmit={(e) => editSupplierForm.handleSubmit(handleEdit)(e)} className="h-140 flex flex-col gap-3 relative">
               <div className="flex flex-col gap-3">
                 <fieldset className="fieldset">
-                  <legend className="fieldset-legend">Bussiness Name</legend>
-                  <input className="input w-full" {...editSupplierForm.register("bussinessName")} type="text" />
+                  <legend className="fieldset-legend text-black">Bussiness Name</legend>
+                  <input className="input w-full bg-white" {...editSupplierForm.register("bussinessName")} type="text" />
                 </fieldset>
                 <fieldset className="fieldset">
-                  <legend className="fieldset-legend">Email</legend>
-                  <input className="input w-full" {...editSupplierForm.register("email")} type="text" />
+                  <legend className="fieldset-legend text-black">Email</legend>
+                  <input className="input w-full bg-white" {...editSupplierForm.register("email")} type="text" />
                 </fieldset>
                 <fieldset className="fieldset">
-                  <legend className="fieldset-legend">Tax Number</legend>
-                  <input className="input w-full" {...editSupplierForm.register("taxNumber")} type="text" />
+                  <legend className="fieldset-legend text-black">Tax Number</legend>
+                  <input className="input w-full bg-white" {...editSupplierForm.register("taxNumber")} type="text" />
                 </fieldset>
                 <fieldset className="fieldset">
-                  <legend className="fieldset-legend">Address</legend>
-                  <input className="input w-full" {...editSupplierForm.register("address")} type="text" />
+                  <legend className="fieldset-legend text-black">Address</legend>
+                  <input className="input w-full bg-white" {...editSupplierForm.register("address")} type="text" />
                 </fieldset>
                 <fieldset className="fieldset">
-                  <legend className="fieldset-legend">Mobile</legend>
-                  <input className="input w-full" {...editSupplierForm.register("mobile")} type="text" />
+                  <legend className="fieldset-legend text-black">Mobile</legend>
+                  <input className="input w-full bg-white" {...editSupplierForm.register("mobile")} type="text" />
                 </fieldset>
               </div>
               {addFn.noResult || addFn.error ? <label className="input-validator text-red-900" htmlFor="user">something went wrong</label> : <></>}
@@ -362,30 +315,30 @@ export default function Suppliers() {
         </div>
       </dialog>
       <dialog id="my_modal_1" ref={modalRef} className="modal text-black">
-        <div className="modal-box">
+        <div className="modal-box bg-white text-black">
           <div className="flex flex-col gap-3">
             <span className="text-2xl">Add Supplier</span>
             <form onSubmit={(e) => newSupplierForm.handleSubmit(submit)(e)} className="h-140 flex flex-col gap-3 relative">
               <div className="flex flex-col gap-3">
                 <fieldset className="fieldset">
-                  <legend className="fieldset-legend">Bussiness Name</legend>
-                  <input className="input w-full" {...newSupplierForm.register("bussinessName")} type="text" />
+                  <legend className="fieldset-legend text-black">Bussiness Name</legend>
+                  <input className="input w-full bg-white" {...newSupplierForm.register("bussinessName")} type="text" />
                 </fieldset>
                 <fieldset className="fieldset">
-                  <legend className="fieldset-legend">Email</legend>
-                  <input className="input w-full" {...newSupplierForm.register("email")} type="text" />
+                  <legend className="fieldset-legend text-black">Email</legend>
+                  <input className="input w-full bg-white" {...newSupplierForm.register("email")} type="text" />
                 </fieldset>
                 <fieldset className="fieldset">
-                  <legend className="fieldset-legend">Tax Number</legend>
-                  <input className="input w-full" {...newSupplierForm.register("taxNumber")} type="text" />
+                  <legend className="fieldset-legend text-black">Tax Number</legend>
+                  <input className="input w-full bg-white" {...newSupplierForm.register("taxNumber")} type="text" />
                 </fieldset>
                 <fieldset className="fieldset">
-                  <legend className="fieldset-legend">Address</legend>
-                  <input className="input w-full" {...newSupplierForm.register("address")} type="text" />
+                  <legend className="fieldset-legend text-black">Address</legend>
+                  <input className="input w-full bg-white" {...newSupplierForm.register("address")} type="text" />
                 </fieldset>
                 <fieldset className="fieldset">
-                  <legend className="fieldset-legend">Mobile</legend>
-                  <input className="input w-full" {...newSupplierForm.register("mobile")} type="text" />
+                  <legend className="fieldset-legend text-black">Mobile</legend>
+                  <input className="input w-full bg-white" {...newSupplierForm.register("mobile")} type="text" />
                 </fieldset>
               </div>
               {addFn.noResult || addFn.error ? <label className="input-validator text-red-900" htmlFor="user">something went wrong</label> : <></>}
